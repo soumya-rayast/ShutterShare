eb465afa0150a89955d7bdb677c11d0679ea2fd2
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-10-09 23:03:57 +0530

    Minor Changes 0.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import GsapTransition from "./components/GsapTransition";
+// import GsapTransition from "./components/GsapTransition";
+import Transition from "./components/Transistion";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
       <Provider store={store}>
         <BrowserRouter>
           <Navbar />
-          <GsapTransition />
+          {/* <GsapTransition /> */}
+          <Transition/>
         </BrowserRouter>
       </Provider>
     </>
---
+++ client/src/components/Transistion.jsx
@@ -0,0 +1,65 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import SellerDashboard from "../pages/SellerDashboard";
+import BuyerDashboard from "../pages/BuyerDashboard";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./ProtectedRoute";
+import Contact from "./Contact";
+import About from "./About";
+
+
+const Transition = () => {
+    const nodeRef = useRef(null);
+    const location = useLocation();
+    useEffect(() => {
+      if (nodeRef.current) {
+        gsap.fromTo(nodeRef.current, { opacity: 0 }, { opacity: 1, duration: 1 });
+      }
+    }, [location]);
+  
+    return (
+      <div ref={nodeRef}>
+        <Toaster />
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={<ProtectedRoute children={<Login />} requiresAuth={false} />}
+          />
+          <Route
+            path="/signup"
+            element={
+              <ProtectedRoute children={<Signup />} requiresAuth={false} />
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <ProtectedRoute children={<Contact />} requiresAuth={false} />
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <ProtectedRoute children={<About />} requiresAuth={false} />
+            }
+          />
+          <Route
+            path="/seller/profile"
+            element={<ProtectedRoute children={<SellerDashboard />} />}
+          />
+          <Route
+            path="/buyer/profile"
+            element={<ProtectedRoute children={<BuyerDashboard />} />}
+          />
+        </Routes>
+      </div>
+    );
+  };
+  
+  export default Transition;
+  
